131e9494c61fe3353478dc25ac47b9f4a688a1c9
amishaalazis <60495662+amishaalazis@users.noreply.github.com>
2021-09-24 08:23:11 +0700

    menyelesaikan video oop dasar pada php yang ke 8 tentang overriding

--- oophp/overriding.php
+++ oophp/overriding.php
@@ -10,17 +10,14 @@ class Produk {
     public $judul, 
             $penulis,
             $penerbit,
-            $harga,
-            $jmlHalaman,
-            $waktuMain;
+            $harga;
 
-    public function __construct( $judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $jmlHalaman=0, $waktuMain = 0) {
+    public function __construct( $judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0) {
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
-        $this->jmlHalaman = $jmlHalaman;
-        $this->waktuMain = $waktuMain;
+ 
     }
 
 
@@ -29,7 +26,7 @@ class Produk {
     }
 
     public function getInfoProduk() {
-        $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+        $str = "{$this->judul}  | {$this->getLabel()} (Rp. {$this->harga})";
     
         return $str;
     }
@@ -37,15 +34,33 @@ class Produk {
 }
 
 class Komik extends Produk {
+    public $jmlHalaman;
+
+    public function __construct( $judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0,  $jmlHalaman = 0) {
+        
+       parent::__construct($judul, $penulis, $penerbit , $harga);
+
+       $this->jmlHalaman = $jmlHalaman;
+    }
+
+
     public function getInfoProduk() {
-        $str = "Komik : " . parent::getInfoProduk() . "- {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
+        $str = "Komik : " . parent::getInfoProduk() . "  - {$this->jmlHalaman} Halaman.";
         return $str;
     }
 }
 
 class Game extends Produk {
+
+    public $waktuMain;
+
+    public function __construct ( $judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $waktuMain = 0) {
+        parent::__construct($judul, $penulis, $penerbit , $harga ) ;
+        $this->waktuMain = $waktuMain;
+    } 
+
     public function getInfoProduk() {
-        $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->waktuMain} Jam.";
+        $str = "Game : " . parent::getInfoProduk() . " ~ {$this->waktuMain} Jam.";
         return $str;
     }
 }
@@ -60,8 +75,8 @@ class CetakInfoProduk {
 
 
 
-$produk1 = new Komik("Naruto","Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, );
-$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 0, 50, );
+$produk1 = new Komik("Naruto","Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 50 );
 
 
 // Komik: Neil Druckman, Sony Computer
