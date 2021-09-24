46416bba5bdfa949a7a93d0093bf637ec0875cfc
amishaalazis <60495662+amishaalazis@users.noreply.github.com>
2021-09-24 13:08:52 +0700

    menyelesaikan video oop dasar pada php tentang constant

---
+++ oophp/constant.php
@@ -0,0 +1,31 @@
+<?php
+
+// define ('NAMA', 'Amisha Alazis');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 20;
+// echo UMUR;
+
+// 
+// echo __LINE__; //menampilkan baris
+// echo __FILE__; //menampilkan alamat file
+
+// function coba() {
+//     return __FUNCTION__;
+//     echo coba();
+// }
+
+class coba {
+    public $kelas = __CLASS__;
+}
+    $obj = new coba;
+    echo $obj->kelas;
+
+
+
+
+
+
+?>
